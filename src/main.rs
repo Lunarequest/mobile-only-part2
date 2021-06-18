92fa27cf77606ec42b724e33d91f222417fd19d9
advaithm <advaith.madhukar@gmail.com>
2021-06-18 14:23:18 +0000

    working website

---
+++ src/main.rs
@@ -0,0 +1,27 @@
+#[macro_use] extern crate rocket;
+use rocket_dyn_templates::Template;
+use std::collections::HashMap;
+use rocket::serde::{Serialize, json::Json};
+
+#[derive(Serialize)]
+struct Message {
+    message: String
+}
+
+ #[get("/")]
+fn index()-> Template {
+    let mut context:HashMap<String,String> = HashMap::new();
+    context.insert("message".to_string(),"you expected a website but it was I dio".to_string());
+    Template::render("index",context)
+ }
+
+#[get("/message")]
+fn message() -> Json<Message> {
+    let message = r#"https://i.imgflip.com/5dpbsy.jpg"#.to_string();
+    Json(Message { message: message })
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build().mount("/",routes![index,message]).attach(Template::fairing())
+}
